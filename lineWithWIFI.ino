3ee1c6e5fe7e8ee9aed2b74e9bf204ebd71896eb
Christopher Colon <103147808+ChristopherColon@users.noreply.github.com>
2024-04-15 22:50:48 -0500

    Add files via upload

---
+++ lineWithWIFI.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <ZumoShield.h>
+#include <ESP8266WiFi.h>
+
+// Wi-Fi credentials
+const char* ssid = "YourWiFiSSID";
+const char* password = "YourWiFiPassword";
+
+ZumoBuzzer buzzer;
+ZumoReflectanceSensorArray reflectanceSensors;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+int lastError = 0;
+const int MAX_SPEED = 400;
+
+void setup() {
+  // Initialize Wi-Fi connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize the reflectance sensors module
+  reflectanceSensors.init();
+
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+
+  // Turn on LED to indicate we are in calibration mode
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // Wait 1 second and then begin automatic sensor calibration
+  delay(1000);
+  int i;
+  for(i = 0; i < 80; i++) {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+      motors.setSpeeds(-200, 200);
+    else
+      motors.setSpeeds(200, -200);
+    reflectanceSensors.calibrate();
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+
+  // Turn off LED to indicate we are through with calibration
+  digitalWrite(13, LOW);
+  buzzer.play(">g32>>c32");
+
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+
+  // Play music and wait for it to finish before we start driving.
+  buzzer.play("L16 cdegreg4");
+  while(buzzer.isPlaying());
+}
+
+void loop() {
+  unsigned int sensors[6];
+  int position = reflectanceSensors.readLine(sensors);
+  int error = position - 2500;
+  int speedDifference = error / 4 + 6 * (error - lastError);
+  lastError = error;
+  int m1Speed = MAX_SPEED + speedDifference;
+  int m2Speed = MAX_SPEED - speedDifference;
+  if (m1Speed < 0)
+    m1Speed = 0;
+  if (m2Speed < 0)
+    m2Speed = 0;
+  if (m1Speed > MAX_SPEED)
+    m1Speed = MAX_SPEED;
+  if (m2Speed > MAX_SPEED)
+    m2Speed = MAX_SPEED;
+  motors.setSpeeds(m1Speed, m2Speed);
+
+  // Send location data over Wi-Fi
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    if (client.connect("your-computer-ip-address", 80)) {
+      client.print("GET /update?location=");
+      client.print(position); // Assuming position is an integer
+      client.println(" HTTP/1.1");
+      client.println("Host: your-computer-ip-address");
+      client.println("Connection: close");
+      client.println();
+      delay(100); // Delay to ensure data is sent properly
+    }
+  }
+}
