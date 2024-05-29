dcaa2b5348a4ee02bf4e75382461551d594b5857
Christopher Colon <103147808+ChristopherColon@users.noreply.github.com>
2024-05-29 13:09:36 -0500

    LineFollower.ino

--- LineFollower.ino
+++ LineFollower.ino
@@ -1,38 +1,22 @@
-/*
- * Demo line-following code for the Pololu Zumo Robot
- *
- * This code will follow a black line on a white background, using a
- * PID-based algorithm.  It works decently on courses with smooth, 6"
- * radius curves and has been tested with Zumos using 30:1 HP and
- * 75:1 HP motors.  Modifications might be required for it to work
- * well on different courses or with different motors.
- *
- * https://www.pololu.com/catalog/product/2506
- * https://www.pololu.com
- * https://forum.pololu.com
- */
-
 #include <Wire.h>
 #include <ZumoShield.h>
 
 ZumoBuzzer buzzer;
-ZumoReflectanceSensorArray reflectanceSensors;
+ZumoReflectanceSensorArray reflectanceSensors(QTR_NO_EMITTER_PIN);
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
-
-// This is the maximum speed the motors will be allowed to turn.
-// (400 lets the motors go at top speed; decrease to impose a speed limit)
 const int MAX_SPEED = 100;
+
+#define LED 13
+#define QTR_THRESHOLD 1500 // Threshold for border detection
 
 unsigned long previousTime = 0; // Variable to track previous time
 
 void setup()
 {
-  // Initialize serial communication
-  Serial.begin(9600);
-
-  // Play a little welcome song
+  Serial.begin(9600); // Initialize serial communication
+  pinMode(LED, OUTPUT);
   buzzer.play(">g32>>c32");
 
   // Initialize the reflectance sensors module
@@ -42,14 +26,13 @@ void setup()
   button.waitForButton();
 
   // Turn on LED to indicate we are in calibration mode
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  digitalWrite(LED, HIGH);
 
   // Wait 1 second and then begin automatic sensor calibration
   // by rotating in place to sweep the sensors over the line
   delay(1000);
   int i;
-  for(i = 0; i < 80; i++)
+  for (i = 0; i < 80; i++)
   {
     if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
       motors.setSpeeds(-200, 200);
@@ -61,10 +44,10 @@ void setup()
     // 80*20 = 1600 ms.
     delay(20);
   }
-  motors.setSpeeds(0,0);
+  motors.setSpeeds(0, 0);
 
   // Turn off LED to indicate we are through with calibration
-  digitalWrite(13, LOW);
+  digitalWrite(LED, LOW);
   buzzer.play(">g32>>c32");
 
   // Wait for the user button to be pressed and released
@@ -72,67 +55,61 @@ void setup()
 
   // Play music and wait for it to finish before we start driving.
   buzzer.play("L16 cdegreg4");
-  while(buzzer.isPlaying());
+  while (buzzer.isPlaying());
 }
 
 void loop()
 {
   unsigned int sensors[6];
-
-  // Get the position of the line.  Note that we *must* provide the "sensors"
-  // argument to readLine() here, even though we are not interested in the
-  // individual sensor readings
   int position = reflectanceSensors.readLine(sensors);
+  bool onLine = (position >= 2400 && position <= 2600);
 
-  // Our "error" is how far we are away from the center of the line, which
-  // corresponds to position 2500.
-  int error = position - 2500;
-
-  // Determine if we are on the line or off the line
-  bool onLine = (sensors[0] > 200 || sensors[1] > 200 || sensors[2] > 200 || sensors[3] > 200 || sensors[4] > 200 || sensors[5] > 200);
-
-// Get current time in seconds
+  // Get current time in seconds
   unsigned long currentTime = millis() / 1000;
 
-  // Send timestamp and status over serial every 1 second
-  if (currentTime - previousTime >= 1) {
-    previousTime = currentTime; // Update previous time
-    if (onLine) {
-      Serial.println(currentTime); // Send timestamp
-      Serial.println("1"); // Send status '1' for on the line
-    } else {
-      Serial.println(currentTime); // Send timestamp
-      Serial.println("0"); // Send status '0' for off the line
-    }
-  }
-  // Get motor speed difference using proportional and derivative PID terms
-  // (the integral term is generally not very useful for line following).
-  // Here we are using a proportional constant of 1/4 and a derivative
-  // constant of 6, which should work decently for many Zumo motor choices.
-  // You probably want to use trial and error to tune these constants for
-  // your particular Zumo and line course.
+  // Calculate error and speed difference
+  int error = position - 2500;
   int speedDifference = error / 4 + 6 * (error - lastError);
-
   lastError = error;
 
-  // Get individual motor speeds.  The sign of speedDifference
-  // determines if the robot turns left or right.
+  // Get motor speeds
   int m1Speed = MAX_SPEED + speedDifference;
   int m2Speed = MAX_SPEED - speedDifference;
 
-  // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
-  // Generally speaking, one motor will always be turning at MAX_SPEED
-  // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
-  // else it will be stationary.  For some applications, you might want to
-  // allow the motor speed to go negative so that it can spin in reverse.
-  if (m1Speed < 0)
-    m1Speed = 0;
-  if (m2Speed < 0)
-    m2Speed = 0;
-  if (m1Speed > MAX_SPEED)
-    m1Speed = MAX_SPEED;
-  if (m2Speed > MAX_SPEED)
-    m2Speed = MAX_SPEED;
+  // Constrain motor speeds
+  if (m1Speed < 0) m1Speed = 0;
+  if (m2Speed < 0) m2Speed = 0;
+  if (m1Speed > MAX_SPEED) m1Speed = MAX_SPEED;
+  if (m2Speed > MAX_SPEED) m2Speed = MAX_SPEED;
 
+  // Set motor speeds
   motors.setSpeeds(m1Speed, m2Speed);
+
+  // Determine direction
+  String direction;
+  if (speedDifference < 0) {
+    direction = "left";
+  } else if (speedDifference > 0) {
+    direction = "right";
+  } else {
+    direction = "straight";
+  }
+
+  // Border detection
+  String leftBorderStatus = (sensors[0] < QTR_THRESHOLD) ? "close" : "safe";
+  String rightBorderStatus = (sensors[5] < QTR_THRESHOLD) ? "close" : "safe";
+
+  // Send timestamp, status, direction, and border distance over serial every 1 second
+  if (currentTime - previousTime >= 1) {
+    previousTime = currentTime; // Update previous time
+    Serial.print(currentTime); // Send timestamp
+    Serial.print(",");
+    Serial.print(onLine ? "1" : "0"); // Send status '1' for on the line and '0' for off the line
+    Serial.print(",");
+    Serial.print(direction); // Send direction
+    Serial.print(",");
+    Serial.print(leftBorderStatus); // Send left border status
+    Serial.print(",");
+    Serial.println(rightBorderStatus); // Send right border status
+  }
 }
